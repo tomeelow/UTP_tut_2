87a6e5898dc50b5b30b688c506603fe4d4b1ec1f
tomeelow <tomeeelow@gmail.com>
2024-10-10 16:18:01 +0200

    Initial project setup

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 //TODO: we need to add missing classes!
 
+//Okay, I will add "Subsctractor" and s31639 will add "Adder"
+
 public class Main {
     public static void main(String[] args) {
         Adder adder = new Adder();
