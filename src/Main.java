4554d549070d88d88a837d7572bbb9475afb15ca
tomeelow <tomeeelow@gmail.com>
2024-10-10 16:06:19 +0200

    Initial project setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Adder adder = new Adder();
+        System.out.println(adder.add(1, 2));
+
+        Substractor substractor = new Substractor;
+        System.out.println(substractor.subsctract(6, 3));
+    }
+}
